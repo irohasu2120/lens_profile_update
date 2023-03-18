d176a507d8eb4a9d6322e2435c0f7600bf2fcbb7
irohasu2120 <halo1991sa@gmail.com>
2023-03-18 14:02:11 +0900

    fix: af2対応

--- force_copy.ps1
+++ force_copy.ps1
@@ -1,4 +1,4 @@
-# lensfunデータをAffinityPhotoへ強制コピーするスクリプトa
+# lensfunデータをAffinityPhotoへ強制コピーするスクリプト
 
-Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "C:\ProgramData\Affinity\Photo\1.0 (Store)\LensProfiles"
-Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "C:\ProgramData\Affinity\Photo\1.0 (Store)\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "C:\ProgramData\Affinity\Photo\2.0 (Store)\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "C:\ProgramData\Affinity\Photo\2.0 (Store)\LensProfiles"
