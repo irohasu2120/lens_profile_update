56048a0ab6e78b37f6698275664338d79a3ffb2d
irohasu2120 <halo1991sa@gmail.com>
2023-03-19 09:31:27 +0900

    affinityPhoto2対応

--- force_copy.ps1
+++ force_copy.ps1
@@ -1,4 +1,7 @@
 # lensfunデータをAffinityPhotoへ強制コピーするスクリプト
 
-Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "C:\ProgramData\Affinity\Photo\2.0 (Store)\LensProfiles"
-Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "C:\ProgramData\Affinity\Photo\2.0 (Store)\LensProfiles"
+# C:\Users\ユーザー名が取得できる
+$userProfile = [System.Environment]::GetFolderPath("UserProfile")
+
+Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
