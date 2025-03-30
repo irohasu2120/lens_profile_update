65b93cb0ef37bc50f8197a0fd269f1d4f6a3d93d
irohasu2120 <halo1991sa@gmail.com>
2025-03-30 13:27:13 +0900

    mod:z lens add.

--- force_copy.ps1
+++ force_copy.ps1
@@ -5,3 +5,4 @@ $userProfile = [System.Environment]::GetFolderPath("UserProfile")
 
 Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
 Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-nikon.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
