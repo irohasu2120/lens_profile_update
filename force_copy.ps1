755eb201d911ab030e88d697b2962b275951a037
irohasu2120 <sa1122photo@yahoo.co.jp>
2025-11-01 17:35:31 +0900

    mod: affinity3.0へ対応

--- force_copy.ps1
+++ force_copy.ps1
@@ -3,6 +3,8 @@
 # C:\Users\ユーザー名が取得できる
 $userProfile = [System.Environment]::GetFolderPath("UserProfile")
 
-Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
-Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
-Copy-Item "..\lensfun\data\db\mil-nikon.xml" -Force "$userProfile\.affinity\Photo\2.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-sony.xml" -Force "$userProfile\.affinity\Affinity\3.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-tamron.xml" -Force "$userProfile\.affinity\Affinity\3.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-nikon.xml" -Force "$userProfile\.affinity\Affinity\3.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\mil-olympus.xml" -Force "$userProfile\.affinity\Affinity\3.0\LensProfiles"
+Copy-Item "..\lensfun\data\db\om-system.xml" -Force "$userProfile\.affinity\Affinity\3.0\LensProfiles"
